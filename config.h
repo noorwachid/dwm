c4cb38616400a6092c8c3c15f5801bcc09a26918
Noor Wachid <wachid1818@gmail.com>
2019-09-16 06:16:30 +0700

    add more shortcut

--- config.h
+++ config.h
@@ -61,12 +61,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *termcmd[]  = { "st", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *wwwcmd[]  = { "firefox", NULL };
+static const char *filecmd[]  = { "st", "-e", "nnn", "-d",  NULL };
+static const char *musiccmd[]  = { "st", "-e", "cmus", NULL };
 
 static Key keys[] = {
   /* modifier                     key        function        argument */
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
   { MODKEY,                       XK_w,      spawn,          {.v = wwwcmd } },
+  { MODKEY,                       XK_e,      spawn,          {.v = filecmd } },
+  { MODKEY,                       XK_a,      spawn,          {.v = musiccmd } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
